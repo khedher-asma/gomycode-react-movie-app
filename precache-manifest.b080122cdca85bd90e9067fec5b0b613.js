2b5dd1f5692e0d20264e406b735ae974820a0ce9
khedher-asma <khedherasma90@gmail.com>
2019-06-26 00:36:22 +0200

    Updates

---
+++ precache-manifest.b080122cdca85bd90e9067fec5b0b613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec7c801b2de780e9f51f0a7b8ade14e",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "cd8a9152ab7ad2ba831c",
+    "url": "/react-movie-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "280d7d5ffd039f668572",
+    "url": "/react-movie-app/static/css/main.ca661e39.chunk.css"
+  },
+  {
+    "revision": "cd8a9152ab7ad2ba831c",
+    "url": "/react-movie-app/static/js/2.9da3dce5.chunk.js"
+  },
+  {
+    "revision": "280d7d5ffd039f668572",
+    "url": "/react-movie-app/static/js/main.9d32b316.chunk.js"
+  },
+  {
+    "revision": "e0c3c7f83a8b3d0de128",
+    "url": "/react-movie-app/static/js/runtime~main.4dfe89d3.js"
+  }
+]);
